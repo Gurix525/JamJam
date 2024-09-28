c76bffc5c94249ac24a8e35587f9f584bb765883
Gurix525 <57004218+Gurix525@users.noreply.github.com>
2024-09-28 18:14:47 +0200

    Inventory max items

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    [SerializeField]
+    private int _maxInventoryAmount = 4;
+
     private Stack<GameObject> _items = new();
+
+    public int ItemsCount => _items.Count;
+
+    public bool CanTakeAdditionalIten => ItemsCount < _maxInventoryAmount;
 
     public void AddItemToStack(GameObject item)
     {
