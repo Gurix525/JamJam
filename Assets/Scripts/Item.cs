7d8a0e532ab5dab5ac812818d1c02fb3894bf3f5
M0hi <>
2024-09-28 16:16:10 +0200

    Message generator ,new object shovel

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,11 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    [SerializeField] private string[] _messages;
+    [SerializeField] private int _minAmount, _maxAmount;
+    public int[] Situations;
+
     public string Name 
     {
         get { return gameObject.name; }
         
         private set { }
     }
+
+    public string[] Messages => _messages;
+    public int MinAmount => _minAmount;
+    public int MaxAmount => _maxAmount;
 }
