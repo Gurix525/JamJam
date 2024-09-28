2b8c094ac125e5ffa4fd03ddabef537a5ee7d133
Wojciech Piech <ilikeahottodogu@gmail.com>
2024-09-28 21:36:52 +0200

    Truck Check

--- Assets/Scripts/Truck.cs
+++ Assets/Scripts/Truck.cs
@@ -6,6 +6,7 @@ public class Truck : Interactable
 {
     private List<GameObject> _items;
     private Inventory _inventory;
+    private bool _taken = false;
 
     public List<GameObject> Items => _items;
 
@@ -17,5 +18,25 @@ public class Truck : Interactable
     public override void Interact()
     {
         _items.Add(_inventory.RemoveItemFromStack());
+    }
+
+    public void Good()
+    {
+        // todo
+    }
+
+    public void Bad()
+    {
+        // todo
+    }
+
+    public bool GetTaken()
+    {
+        return _taken;
+    }
+
+    public List<GameObject> GetItems()
+    {
+        return _items;
     }
 }
