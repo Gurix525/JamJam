b03aace913825db62a48d62bb9f1e980faa68ac1
Gurix525 <57004218+Gurix525@users.noreply.github.com>
2024-09-28 14:24:56 +0200

    Add interaction

--- Assets/Interactor.cs
+++ Assets/Interactor.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Interactor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float _maxInteractionDistance = 1F;
+
+    private Interactable[] _interactables;
+    private Interactable _targetInteractable;
+
+    private void Start()
     {
-        
+        _interactables = Object.FindObjectsOfType<Interactable>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        _interactables = _interactables
+            .OrderBy(x => Vector2.Distance(x.transform.position, transform.position))
+            .ToArray();
+        if (_interactables.Length == 0)
+        {
+            _targetInteractable = null;
+        }
+        if (Vector2.Distance(_interactables.First().transform.position, transform.position) < _maxInteractionDistance)
+        {
+            _targetInteractable = _interactables.First();
+        }
+        else
+        {
+            _targetInteractable = null;
+        }
+    }
+
+    public void OnInteract(InputValue value)
+    {
+        if (_targetInteractable == null)
+            return;
+        _targetInteractable.Interact();
     }
 }
