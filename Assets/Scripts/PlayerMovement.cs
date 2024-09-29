8d69c74b4715b5f8449a6725775d947d2239ff77
Bartosz <warzech2000@gmail.com>
2024-09-29 03:20:42 +0200

    ale se chodzi

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,13 +10,25 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _inputDirection;
     private Rigidbody _rb;
 
-    public float Speed = 5f;
     public float baseSpeed = 5f;
+    public float maxSpeed = 15f;
+    public float acceleration = 1f;
     public float speed;
     private Camera _mainCamera;
     private Inventory _inventory;
     public float speedPercentsPerItem;
 
+    private Animator _animator;
+    private bool _holdingItem;
+    private int inventoryCount = 0;
+
+    public float transitionDuration = 1f;
+    private float currentSpeed = 0f;
+
+    private float speedModifier = 1f;
+    
+
+    
 
     public void Start()
     {
@@ -24,11 +36,12 @@ public class PlayerMovement : MonoBehaviour
         _mainCamera = Camera.main;
         speed = baseSpeed;
         _inventory = GameObject.FindObjectOfType<Inventory>();
+        _animator = GetComponentInChildren<Animator>();
+
     }
 
     public void Update()
     {
-        _rb.velocity = new Vector3(_inputDirection.x, 0, _inputDirection.y ) * Speed;
         Vector3 forward = _mainCamera.transform.forward;
         Vector3 right = _mainCamera.transform.right;
 
@@ -38,16 +51,43 @@ public class PlayerMovement : MonoBehaviour
         forward.Normalize();
         right.Normalize();
 
+
         Vector3 moveDirection = (forward * _inputDirection.y + right * _inputDirection.x).normalized;
 
-        _rb.velocity = moveDirection * speed;
+
+        if (moveDirection.magnitude > 0.1f)
+        {
+            currentSpeed = Mathf.Min(currentSpeed + acceleration * Time.deltaTime, maxSpeed);
+        }
+        else
+        {
+            currentSpeed = baseSpeed;
+        }
+
+        _rb.velocity = moveDirection * currentSpeed;
 
         if (moveDirection != Vector3.zero)
         {
             Quaternion lookRotation = Quaternion.LookRotation(moveDirection);
             transform.rotation = lookRotation;
         }
+
+        if (_animator)
+        {
+            UpdateAnimator();
+        }
     }
+
+    private void UpdateAnimator()
+    {
+        bool moving = _rb.velocity.magnitude > 1f;
+        inventoryCount = _inventory.GetStackLength();
+        _ = (inventoryCount != 0) ? _holdingItem = true : _holdingItem = false;
+
+        _animator.SetFloat("velocity", _rb.velocity.magnitude);
+        _animator.SetBool("holdingItems", _holdingItem);
+    }
+
 
     public void OnMove(InputValue value)
     {
@@ -56,6 +96,6 @@ public class PlayerMovement : MonoBehaviour
 
     public void UpdateSpeed()
     {
-        speed = baseSpeed * (1-(_inventory.GetStackLength() * speedPercentsPerItem));
+        speedModifier = 1f - (float)inventoryCount * speedPercentsPerItem;
     }
 }
